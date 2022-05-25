bd1be706f1ca089adaaecaf18bb3c0ddd078491e
Jordan <87206931+Jordan1678@users.noreply.github.com>
2022-05-25 09:51:18 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,58 @@
+// Setup Motor and Ports
 #pragma config(Motor,  port2,           RightRear,     tmotorVex393, openLoop, reversed)
 #pragma config(Motor,  port3,           RightFront,     tmotorVex393, openLoop, reversed)
 #pragma config(Motor,  port4,           LeftRear,     tmotorVex393, openLoop)
 #pragma config(Motor,  port5,           LeftFront,     tmotorVex393, openLoop)
 #pragma config(Motor,  port6,           ArmLift,     tmotorVex393, openLoop)
+#pragma config(Motor,  PORT_NUM_HERE,           Elbow,     tmotorVex393, openLoop)
 #pragma config(Motor,  port8,           Bucket,     tmotorVex393, openLoop)
 
+// Drive Axis
+char DriveX = Ch4;
+char DriveY = Ch3;
 
-// Arm Control Map
-char ArmUP = Btn5U;
-char ArmDown = Btn5D;
+// Arm Axis
+char Arm = Ch2
+
+// Elbow Control Map
+char ElbowUP = Btn5U;
+char ElbowDOWN = Btn5D;
 
 // Bucket Control Map
+char BucketUP = Btn6U;
+char BucketDOWN = Btn6D;
 
 task main ()
 {
 
 	while(1 == 1)
-	{
-		motor[RightRear]  = (vexRT[Ch2] + vexRT[Ch1] * -1)/2;  // (y + x)/2
-		motor[RightFront] = (vexRT[Ch2] + vexRT[Ch1] * -1)/2;  // (y - x)/2
-		motor[LeftRear] = (vexRT[Ch2] - vexRT[Ch1] * -1)/2;
-		motor[LeftFront] = (vexRT[Ch2] - vexRT[Ch1] * -1)/2;
+	{	
+		// Drive Control
+		motor[RightRear]  = (vexRT[DriveY] + vexRT[DriveX] * -1)/2;
+		motor[RightFront] = (vexRT[DriveY] + vexRT[DriveX]] * -1)/2;
+		motor[LeftRear] = (vexRT[DriveY] - vexRT[DriveX]] * -1)/2;
+		motor[LeftFront] = (vexRT[DriveY] - vexRT[DriveX]] * -1)/2;
 
 		// Lift Arm
-		if(vexRT[ArmUP] == 1)       			//If button is pressed...
-		{
-			motor[ArmLift] = 32;    				//...raise the arm.
-			
-		} else if(vexRT[ArmDown] == 1) { 	//Else, if button is pressed...
-			motor[ArmLift] = -32;   				//...lower the arm.
-			
-		} else {                  				//Else (neither button is pressed)...
-			motor[ArmLift] = 0;      				//...stop the arm.
-			
+		motor[ArmLift] = vexRT[Arm]/2;
+		
+		// Bend Elbow
+		if (vexRT[ElbowUP] == 1) {
+			motor[Elbow] = 127;
+		
+		} else if (vexRT[ElbowDOWN] == 1) {
+			motor[Elbow] = -127;
+		
+		} else {
+			motor[Elbow] = 0;
+		
 		}
 
-// Open, close or do not more claw
-		if(vexRT[Btn6U] == 1) {       	//If Button 6U is pressed...
+		// Rotate Bucket
+		if (vexRT[Btn6U] == 1) {       	//If Button 6U is pressed...
 			motor[Bucket] = 65;  			//...close the gripper.
 			
-		} else if(vexRT[Btn6D] == 1) { 	//Else, if button 6D is pressed...
+		} else if (vexRT[Btn6D] == 1) { 	//Else, if button 6D is pressed...
 			motor[Bucket] = -65; 			//...open the gripper.
 			
 		} else {                      	//Else (neither button is pressed)...
